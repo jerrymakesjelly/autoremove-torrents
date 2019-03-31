bbabd6cd0d29b7701997060e49c2f56bc7f5f589
Gan Zhaoyu <ganzhaoyu037@gmail.com>
2019-03-31 15:15:37 +0800

    Used mocker to speed up tester

--- pytest/test_main/test_main.py
+++ pytest/test_main/test_main.py
@@ -1,13 +1,18 @@
 import os
+import requests_mock
 from autoremovetorrents import logger
 from autoremovetorrents.main import pre_processor
 
 # Logger
 lg = logger.register(__name__)
 
-def test_main():
+def test_main(requests_mock):
     basic_dir = os.path.realpath(os.path.dirname(__file__))
     lg.info('Basic directory: %s' % basic_dir)
+
+    # Mock requests
+    requests_mock.get('mock://qbittorrent/login', text='Fails.')
+    requests_mock.get('mock://transmission/transmission/rpc', status_code=401)
     
     # Open file of command lines
     with open(os.path.join(basic_dir, 'command_lines.txt')) as f:
