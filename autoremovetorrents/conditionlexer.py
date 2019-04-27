5f81c8096bd5d3411b774944ffa0bf431c217761
Gan Zhaoyu <ganzhaoyu037@gmail.com>
2019-04-27 17:35:12 +0800

    Fixed a bug where the keyword and/or could not be recognized in the parser

--- autoremovetorrents/conditionlexer.py
+++ autoremovetorrents/conditionlexer.py
@@ -3,19 +3,20 @@ from . import logger
 from .exception.illegalcharacter import IllegalCharacter
 
 class ConditionLexer(object):
-    tokens = (
+    reserved = {
+        'and': 'AND',
+        'or': 'OR'
+    }
+    tokens = [
         'CONDITION', 'NUMBER',
         'LT', 'GT',
         'AND', 'OR',
         'LPAREN', 'RPAREN',
-    )
+    ] + list(reserved.values())
 
     # Regular expression of tokens
-    t_CONDITION = r'[a-zA-z_]+'
     t_LT = r'<'
     t_GT = r'>'
-    t_AND = r'and'
-    t_OR = r'or'
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
     
@@ -25,6 +26,11 @@ class ConditionLexer(object):
     def t_NUMBER(self, t):
         r'\d+(\.\d+)?'
         t.value = float(t.value)
+        return t
+
+    def t_CONDITION(self, t):
+        r'[a-zA-Z_]+'
+        t.type = self.reserved.get(t.value, 'CONDITION')
         return t
     
     def t_error(self, t):
