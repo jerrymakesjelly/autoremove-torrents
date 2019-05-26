16b244570bae98d8797b53ba036d658bb8ae49ab
Gan Zhaoyu <ganzhaoyu037@gmail.com>
2019-05-26 15:27:46 +0800

    Supported Transmission labels
    Closes #24.

--- autoremovetorrents/client/transmission.py
+++ autoremovetorrents/client/transmission.py
@@ -76,7 +76,7 @@ class Transmission(object):
     def torrent_properties(self, torrent_hash):
         result = self._make_transmission_request('torrent-get',
             {'ids': [torrent_hash],
-            'fields': ['hashString', 'name', 'trackers', 'status', 'totalSize', 'uploadRatio', 'uploadedEver', 'addedDate', 'secondsSeeding', 'isStalled', 'error']}
+            'fields': ['hashString', 'name', 'trackers', 'status', 'totalSize', 'uploadRatio', 'uploadedEver', 'addedDate', 'secondsSeeding', 'isStalled', 'error', 'labels']}
             )
         if len(result['torrents']) == 0: # No such torrent
             raise NoSuchClient("No such torrent of hash '%s'." % torrent_hash)
@@ -85,6 +85,8 @@ class Transmission(object):
         torrent_obj = Torrent()
         torrent_obj.hash = torrent['hashString']
         torrent_obj.name = torrent['name']
+        if 'labels' in torrent:
+            torrent_obj.category = torrent['labels']
         torrent_obj.tracker = [tracker['announce'] for tracker in torrent['trackers']]
         torrent_obj.status = Transmission._judge_status(torrent['status'], torrent['error'])
         torrent_obj.size = torrent['totalSize']
