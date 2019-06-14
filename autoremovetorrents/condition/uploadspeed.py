677ce374ffd9acc355f5f8e6f7c895d78af94fbe
Gan Zhaoyu <ganzhaoyu037@gmail.com>
2019-06-15 00:05:52 +0800

    Added downloading status to upload_speed condition

--- autoremovetorrents/condition/uploadspeed.py
+++ autoremovetorrents/condition/uploadspeed.py
@@ -12,7 +12,8 @@ class UploadSpeedCondition(Condition):
         for torrent in torrents:
             # Note: The speed unit is KiB/s
             # Note: This condition is only available for the uploading torrents
-            if torrent.status == TorrentStatus.Uploading and self.compare(torrent.upload_speed, self._upspeed * 1024, self._comparer):
+            if (torrent.status == TorrentStatus.Uploading or torrent.status == TorrentStatus.Downloading) \
+                and self.compare(torrent.upload_speed, self._upspeed * 1024, self._comparer):
                 self.remove.add(torrent)
             else:
                 self.remain.add(torrent)
