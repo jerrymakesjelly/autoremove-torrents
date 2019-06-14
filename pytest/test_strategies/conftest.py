eeb9b72e2dabfa80ff1264cb7758cd07b594b477
Gan Zhaoyu <ganzhaoyu037@gmail.com>
2019-06-15 00:06:18 +0800

    Added test cases for up/dl speed condition

--- pytest/test_strategies/conftest.py
+++ pytest/test_strategies/conftest.py
@@ -28,6 +28,8 @@ def test_data():
         torrent_obj.uploaded = torrent['uploaded']
         torrent_obj.create_time = torrent['added_on']
         torrent_obj.seeding_time = torrent['seeding_time']
+        torrent_obj.upload_speed = torrent['upspeed']
+        torrent_obj.download_speed = torrent['dlspeed']
         input_torrents.append(torrent_obj)
 
     return input_torrents
