7229e89844d7615c55029387a3dd02f8cded83a5
jerrymakesjelly <ganzhaoyu037@sina.com>
2020-02-01 23:21:04 +0800

    Fix master document reference

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# The master toctree document.
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
