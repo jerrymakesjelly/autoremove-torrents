583dd2b2b3315e2516d065606a2cd0adcbac574b
jerrymakesjelly <ganzhaoyu037@sina.com>
2020-02-14 00:18:36 +0800

    Fix missing attribute in uTorrent

--- autoremovetorrents/client/utorrent.py
+++ autoremovetorrents/client/utorrent.py
@@ -99,6 +99,7 @@ class uTorrent(object):
                 torrent_obj.status = uTorrent._judge_status(torrent[1], torrent[4])
                 torrent_obj.size = torrent[3]
                 torrent_obj_ratio = torrent[7]/1000
+                torrent_obj.uploaded = torrent[6]
                 torrent_obj.seeding_time = torrent[6]
                 torrent_obj.upload_speed = properties['ulrate']
                 torrent_obj.download_speed = properties['dlrate']
