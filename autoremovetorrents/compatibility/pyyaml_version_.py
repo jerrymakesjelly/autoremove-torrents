ddafca6f195f25879bf9eff9759ff05d05715fa1
jerrymakesjelly <ganzhaoyu037@sina.com>
2022-04-15 23:47:31 +0800

    Downgraded pyyaml to 5.3 for earlier version of Python

--- autoremovetorrents/compatibility/pyyaml_version_.py
+++ autoremovetorrents/compatibility/pyyaml_version_.py
@@ -4,4 +4,4 @@ import sys
 # To make our program still available in earlier versions of Python,
 # we select an older version of pyyaml manually. (Not Recommend)
 
-PYYAML_VERSION = 'pyyaml' if sys.version_info >= (3, 6, 0) else 'pyyaml==5.4.1'
+PYYAML_VERSION = 'pyyaml' if sys.version_info >= (3, 6, 0) else 'pyyaml==5.3'
